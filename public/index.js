04adebd47e2916a1cedcfbd489b929bd63548c5a
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-04-17 00:27:19 -0700

    minor edit

--- public/index.js
+++ public/index.js
@@ -7,7 +7,7 @@ fetch("/api/tasks")
     .then(tasks => {
         console.log(tasks);
         tasks.forEach(task => {
-                console.log("Adding h2 for task:", task.task_body);
-                container.innerHTML += `<div class="task-container"><button id="${task.id}">Edit</button><h2>${task.task_body}</h2></div>`;
+            console.log("Adding h2 for task:", task.task_body);
+            container.innerHTML += `<div class="task-container"><button id="${task.id}">Edit</button><h2>${task.task_body}</h2></div>`;
             });
     });
